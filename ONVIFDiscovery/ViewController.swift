2ce559987a1b4291b7508149675c974d8b7f8a5f
ms2138 <m38371@gmail.com>
2019-11-30 14:57:09 -0800

    Initial commit

---
+++ ONVIFDiscovery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ONVIFDiscovery
+//
+//  Created by admin on 2019-11-28.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
