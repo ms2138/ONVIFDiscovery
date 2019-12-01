481231489ea475acd74535a55e4c49559938ba0b
ms2138 <m38371@gmail.com>
2019-11-30 20:11:59 -0800

    Add ONVIFQueryService class

---
+++ ONVIFDiscovery/ONVIFQueryService.swift
@@ -0,0 +1,12 @@
+//
+//  ONVIFQueryService.swift
+//  ONVIFDiscovery
+//
+//  Created by mani on 2019-11-30.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import Foundation
+
+class ONVIFQueryService {
+}
