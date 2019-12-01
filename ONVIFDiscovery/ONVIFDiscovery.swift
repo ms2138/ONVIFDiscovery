5749404c39cd490c4349036eac97d5ccbcb94d55
ms2138 <m38371@gmail.com>
2019-11-30 20:21:47 -0800

    Add ONVIFDiscovery struct

---
+++ ONVIFDiscovery/ONVIFDiscovery.swift
@@ -0,0 +1,12 @@
+//
+//  ONVIFDiscovery.swift
+//  ONVIFDiscovery
+//
+//  Created by mani on 2019-11-30.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import Foundation
+
+struct ONVIFDiscovery {
+}
