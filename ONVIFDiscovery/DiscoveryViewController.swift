dd1dac411a46e7ef6352a102a7ae26b77cee6e19
ms2138 <m38371@gmail.com>
2019-12-01 15:17:30 -0800

    Change DiscoveryViewController to UITableViewController

--- ONVIFDiscovery/DiscoveryViewController.swift
+++ ONVIFDiscovery/DiscoveryViewController.swift
@@ -1,7 +1,7 @@
 
 import UIKit
 
-class DiscoveryViewController: UIViewController {
+class DiscoveryViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
