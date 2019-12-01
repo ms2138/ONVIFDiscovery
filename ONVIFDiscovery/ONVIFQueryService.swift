da3e56a2216f64ca73c16577e5064e035a2e8bc4
ms2138 <m38371@gmail.com>
2019-11-30 20:17:48 -0800

    Add session and broadcastConnection properties.  Add init and performDataTask methods to ONVIFQueryService

--- ONVIFDiscovery/ONVIFQueryService.swift
+++ ONVIFDiscovery/ONVIFQueryService.swift
@@ -1,12 +1,22 @@
-//
-//  ONVIFQueryService.swift
-//  ONVIFDiscovery
-//
-//  Created by mani on 2019-11-30.
-//  Copyright © 2019 home. All rights reserved.
-//
 
 import Foundation
 
 class ONVIFQueryService {
+    let session: URLSession
+    private var broadcastConnection: UDPBroadcastConnection!
+
+    init() {
+        self.session = URLSession(configuration: URLSessionConfiguration.ephemeral)
+    }
+
+    func performDataTask(for request: ONVIFRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        var urlRequest = URLRequest.init(url: request.url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.httpBody = request.body
+
+        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+            completion(data, response, error)
+        }
+        task.resume()
+    }
 }
