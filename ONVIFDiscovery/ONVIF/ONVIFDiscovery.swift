5dc33e9b71b5a08d8bdfd34ca18fade2055281a8
ms2138 <m38371@gmail.com>
2019-12-01 19:10:46 -0800

    Add messageLabel property.  Add init method

--- ONVIFDiscovery/ONVIF/ONVIFDiscovery.swift
+++ ONVIFDiscovery/ONVIF/ONVIFDiscovery.swift
@@ -1,10 +1,3 @@
-//
-//  ONVIFDiscovery.swift
-//  ONVIFDiscovery
-//
-//  Created by mani on 2019-11-30.
-//  Copyright © 2019 home. All rights reserved.
-//
 
 import Foundation
 
--- ONVIFDiscovery/TableBackgroundView.swift
+++ ONVIFDiscovery/TableBackgroundView.swift
@@ -2,4 +2,19 @@
 import UIKit
 
 class TableBackgroundView: UIView {
+    let messageLabel: UILabel
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        self.messageLabel = UILabel(frame: .zero)
+        self.messageLabel.textColor = UIColor.darkGray
+        self.messageLabel.font = UIFont.systemFont(ofSize: 20.0)
+        self.messageLabel.numberOfLines = 0
+        self.messageLabel.textAlignment = .center
+
+        super.init(frame: frame)
+    }
 }
