ee318b1c79e09852e0e28690d0aaafbeec544a65
ms2138 <m38371@gmail.com>
2019-12-01 19:14:07 -0800

    Add actionButton and activityIndicator properties to TableBackgroundView

--- ONVIFDiscovery/TableBackgroundView.swift
+++ ONVIFDiscovery/TableBackgroundView.swift
@@ -3,6 +3,8 @@ import UIKit
 
 class TableBackgroundView: UIView {
     let messageLabel: UILabel
+    let actionButton: UIButton
+    let activityIndicator: UIActivityIndicatorView
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -14,6 +16,11 @@ class TableBackgroundView: UIView {
         self.messageLabel.font = UIFont.systemFont(ofSize: 20.0)
         self.messageLabel.numberOfLines = 0
         self.messageLabel.textAlignment = .center
+
+        self.actionButton = UIButton(type: .roundedRect)
+
+        self.activityIndicator = UIActivityIndicatorView(style: .large)
+        self.activityIndicator.color = .darkGray
 
         super.init(frame: frame)
     }
