67cdcdb084fdbfc4b2ad1949c237cef63679aa2f
ms2138 <m38371@gmail.com>
2019-12-01 22:28:16 -0800

    Add showBackgroundView and hideBackgroundView methods to NoContentBackground protocol.  Add NoContentBackground extension to implement defaults for showBackgroundView and hideBackgroundView methods

--- ONVIFDiscovery/NoContentBackground.swift
+++ ONVIFDiscovery/NoContentBackground.swift
@@ -4,6 +4,21 @@ import UIKit
 protocol NoContentBackground where Self: UIViewController {
     var tableView: UITableView! { get set }
     var backgroundView: TableBackgroundView { get }
+
+    func showBackgroundView()
+    func hideBackgroundView()
+}
+
+extension NoContentBackground {
+    func showBackgroundView() {
+        tableView.separatorStyle = .none
+        self.tableView.backgroundView?.isHidden = false
+    }
+
+    func hideBackgroundView() {
+        tableView.separatorStyle = .singleLine
+        self.tableView.backgroundView?.isHidden = true
+    }
 }
 
 
